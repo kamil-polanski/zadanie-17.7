94e4df0e3b76b8110795e532e20fd69e0790f31a
Kamil Polanski <polanski88@gmail.com>
2019-06-01 20:33:17 +0200

    Fix err disp for fs.readdir

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const colors = require(`colors`);
 const fs = require(`fs`);
 fs.readdir('.', function(err, items) {
+    if (err) {
+        console.log(err);
+    }
     console.log(`Zawartość folderu:`.yellow);
     console.log(items);
     fs.writeFile(`items.txt`, items, function(err) {
